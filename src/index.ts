14a69c34a480182836ef9c720d16aa48c2af0ee8
Olegzandr VD <alex@oresoftware.com>
2018-05-08 01:38:43 -0700

    init

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+'use strict';
+
+import * as util from "util";
+
+
+const matchAny = [/:[0-9]/];
+
+const notMatchAny = [
+  /bootstrap_node\.js/,
+  /Function\.Module\.runMain/,
+  /process\._tickCallback/,
+  /at Module\._compile/
+];
+
+export const settings = {
+    fullTrace: false,
+    excludeNodeModules: false
+};
+
+export const getUsefulStack = function (e: Error) {
+  
+  let err = (e && e.stack || e) as string;
+  
+  if (typeof err !== 'string') {
+    // breakLength => keep everything on one line
+    err = util.inspect(err, {breakLength: Infinity});
+  }
+  
+  return String(err).split('\n')
+  .filter(function (v, i) {
+    if (i < 2) return true;
+    return matchAny.some(function (r) {
+      return r.test(v);
+    })
+  })
+  .filter(function (v, i) {
+    if (i < 2) return true;
+    return !notMatchAny.some(function (r) {
+      return r.test(v);
+    });
+  })
+  .join('\n')
+};
+
+
+
+export const getCleanTrace = getUsefulStack;
